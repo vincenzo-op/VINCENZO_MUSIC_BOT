c3a56e5c277da0fb1dbf803f4581fd9395319e8b
✦✧✧νιиϲєиzο✧✧✦ <90239565+vincenzo-op@users.noreply.github.com>
2021-09-09 11:03:18 +0530

    Create admins.py

---
+++ vincenzo/helpers/admins.py
@@ -0,0 +1,22 @@
+from typing import List
+
+from pyrogram.types import Chat, User
+
+from vincenzo.function.admins import get as gett
+from vincenzo.function.admins import set
+
+
+async def get_administrators(chat: Chat) -> List[User]:
+    get = gett(chat.id)
+
+    if get:
+        return get
+    administrators = await chat.get_members(filter="administrators")
+    to_set = []
+
+    for administrator in administrators:
+        # if administrator.can_manage_voice_chats:
+        to_set.append(administrator.user.id)
+
+    set(chat.id, to_set)
+    return await get_administrators(chat)
