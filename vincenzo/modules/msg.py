35ea786ca483f7edf15ef97167188b7553f76e88
✦✧✧νιиϲєиzο✧✧✦ <90239565+vincenzo-op@users.noreply.github.com>
2021-09-09 20:04:49 +0530

    Create msg.py

---
+++ vincenzo/modules/msg.py
@@ -0,0 +1,78 @@
+from vincenzo.config import ASSISTANT_NAME, OWNER, PROJECT_NAME
+
+
+class Messages:
+    HELP_MSG = [
+        ".",
+        f"""
+**:) HY WELCOME BACK TO [{PROJECT_NAME}](https://telegra.ph/file/744ec1f5f15768fd3cc0b.jpg)
+!! {PROJECT_NAME} CAN PLAY MUSIC IN GROUP OR CHANNEL VOICE CHATS....
+(>_<)Assistant Name >> @{ASSISTANT_NAME}\n\n ** CLICK NEXT FOR MORE OPTIONS...**
+""",
+        f"""
+**!!SETTINGS!!**
+1)Make Bot As Admin
+2) Start Voice Chat / Vcg
+3) Send Command /userbotjoin
+• If Assistant Bot Joins Enjoy Music,
+• If Assistant Bot Not Joined Please Add @{ASSISTANT_NAME} To Your Group And Try Again
+**For Music Play Channel ??**
+1) Make Bot As Channel Admin
+2) Submit /userbotjoinchannel In Linked Group
+3) Now Send Command In Linked Group
+""",
+        """
+**COMMANDS**
+**=>> PLAY SONGS..**
+• /play (song name) - To Play the song you requested via youtube
+• /ytplay (song name) - To Play the song you requested via youtube
+• /yt (song name) - To Play the song you requested via youtube
+• /p (song name) - To Play the song you requested via youtube
+• /lplay - Reply songs on gc will be automatically played on vcg
+• /player: Open the Player settings menu
+• /skip: Skips the current track
+• /pause: Pause track
+• /resume: Resume a paused track
+• /end: ​​??Stop media playback
+• /current: Displays the currently playing track
+• /playlist: Displays a playlist
+All Commands Can Be Used Except Command /player /skip /pause /resume /end Only For Group Admin
+**==>>Download Songs ??**
+• /song [song name]: Download song audio from youtube
+""",
+        f"""
+***=>> Music Play Channel ??**
+?? For linked group admins only:
+• /cplay (song name) - play the song you requested
+• /cplaylist - Show currently playing list
+• /cccurrent - Show currently playing
+• /cplayer - open the music player settings panel
+• /cpause - pause song playback
+• /cresume - resume song playback
+• /cskip - play next song
+• /cend - stop music playing
+• /userbotjoinchannel - invite assistants to your chat
+--> If You Don't Like Playing In Linked Groups:
+1) Get your Channel ID.
+2) Create a Group With Title: Music Channel: YOUR_CHANNEL ID
+3) Add Bot As Channel Admin With Full Permission
+4) Add @{ASSISTANT_NAME} To Channel As admin.
+5) Just Send Order In Your Group
+***=>> More Tools !!**
+- /admincache: Update your Group Admin Info. Try If Bot Doesn't Recognize Admin
+- /userbotjoin: Invite @{ASSISTANT_NAME} Userbot To Your Group
+""",
+        f"""HY DUDE I AM  [{PROJECT_NAME}](https://telegra.ph/file/744ec1f5f15768fd3cc0b.jpg)
+MADE BY--> {OWNER}
+
+・✦▭▭▭▭✧◦✦◦✧▭▭▭▭✦ ・
+--> I have many features for those of you who like songs
+--> Playing songs in the group
+--> Playing songs on the channel
+--> Download songs
+--> Looking for youtube links
+
+・✦▭▭▭▭✧◦✦◦✧▭▭▭▭✦ ・
+--> Click the help button for more information
+""",
+     ]
